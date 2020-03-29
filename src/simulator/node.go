af0c302da8245e928065c6377a08e74618acb632
aleksandr_desktop <aleks.stevens@protonmail.com>
2020-03-29 16:38:16 -0700

    forgot to push change on last commit

--- src/simulator/node.go
+++ src/simulator/node.go
@@ -7,7 +7,6 @@ type Node interface {
 	Conn() chan string
 	Tokens() float64
 	Sign([]byte) []byte
-	Verify([]byte, []byte, *rsa.PublicKey) bool
 	PublicKey() *rsa.PublicKey
 	JoinVerification(chan string, chan string) chan bool
 	EnterContract(chan string, chan string) chan bool
